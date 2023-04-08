78bb30bc7de87a5c09eb92862d89deab59fa9455
ajcapacete <ajcapacete@gmail.com>
2023-04-08 21:49:06 +0800

    [master] Working staggered menu item animation

--- src/FullScreenNav.tsx
+++ src/FullScreenNav.tsx
@@ -8,29 +8,29 @@ import { useRef } from "react";
 
 const menu = {
     open: {
-        scale: 1,
-        opacity: 100,
+        scaleY: 1,
+        // opacity: 100,
         transition: {
-            // type: "spring",
-            // stiffness: 20,
-            // restDelta: 2,
+            type: "spring",
+            stiffness: 20,
+            restDelta: 2,
         },
     },
     closed: {
-        scale: 0,
-        opacity: 0,
+        scaleY: 0,
+        // opacity: 0,
         transition: {
-            // delay: 0.5,
-            // type: "spring",
-            // stiffness: 400,
-            // damping: 40,
+            delay: 0.5,
+            type: "spring",
+            stiffness: 400,
+            damping: 40,
         },
     }
 };
 
-const menuItems = {
+const menuLi = {
     open: {
-        y: 0,
+        y: 50,
         opacity: 100,
         transition: {
             y: {
@@ -40,7 +40,7 @@ const menuItems = {
         }
     },
     closed: {
-        y: 100,
+        y: 0,
         opacity: 0,
         transition: {
             y: {
@@ -50,7 +50,7 @@ const menuItems = {
     }
 };
 
-const staggerVariants = {
+const menuUl = {
     open: {
         transition: {
             staggerChildren: 0.07,
@@ -78,28 +78,29 @@ function FullScreenNav() {
 
     return (
         <motion.div
-            // initial={false}
-            animate={navOpen ? 'open' : 'closed'}
             variants={menu}
             className={`fixed top-0 bottom-0 left-0 right-0 z-40 justify-center bg-black/50 backdrop-blur-lg origin-top`}
-            // initial={{ opacity: 0, display: 'none' }}
+            animate={navOpen ? 'open' : 'closed'}
         >
-            <div className="flex flex-col items-center justify-center w-screen h-screen">
-                <motion.ul variants={staggerVariants} className="flex flex-col gap-8">
-                    {CONFIG.menus.map((i, idx) => (
-                        <motion.li
-                            variants={menuItems}
-                            animate={navOpen ? 'open' : 'closed'}
-                            key={idx}
-                            whileHover={{ scale: 1.1 }}
-                            whileTap={{ scale: 0.95 }}
-                            className="text-3xl font-light text-center text-gray-300"
-                        >
-                            <Link to={i.url} onClick={() => setNavOpen(false)}>{i.label}</Link>
-                        </motion.li>
-                    ))}
-                </motion.ul>
-            </div>
+                <div className="flex flex-col items-center justify-center w-screen h-screen">
+                    <motion.ul
+                        variants={menuUl}
+                        animate={navOpen ? 'open' : 'closed'}
+                        className="flex flex-col gap-8"
+                    >
+                        {CONFIG.menus.map((i, idx) => (
+                            <motion.li
+                                variants={menuLi}
+                                key={idx}
+                                whileHover={{ scale: 1.1 }}
+                                whileTap={{ scale: 0.95 }}
+                                className="text-3xl font-light text-center text-gray-300"
+                            >
+                                <Link to={i.url} onClick={() => setNavOpen(false)}>{i.label}</Link>
+                            </motion.li>
+                        ))}
+                    </motion.ul>
+                </div>
         </motion.div>
     );
 }
