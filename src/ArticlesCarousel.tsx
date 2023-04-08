af883e42315560200ae42e6ff95fb496eac59ced
ajcapacete <ajcapacete@gmail.com>
2023-04-08 18:30:28 +0800

    [master] Working ArticlesCarousel

--- src/ArticlesCarousel.tsx
+++ src/ArticlesCarousel.tsx
@@ -1,3 +1,10 @@
+import { Navigation, Pagination, EffectFade } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/effect-fade';
+
 import data from "../public/articles.json";
 import ArticleCarouselItem from "./ArticleCarouselItem";
 
@@ -5,9 +12,22 @@ import ArticleCarouselItem from "./ArticleCarouselItem";
 function ArticlesCarousel() {
     return (
         <section>
-            {data.records.map((i, idx) => (
-                <ArticleCarouselItem key={idx} data={i} />)
-            )}
+            {/* <Swiper
+                spaceBetween={0}
+                slidesPerView={1}
+            > */}
+            <Swiper
+                modules={[Navigation, Pagination, EffectFade]}
+                navigation
+                pagination={{ clickable: true }}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+                effect="fade"
+            >
+                {data.records.map((i, idx) => (
+                    <SwiperSlide key={idx}><ArticleCarouselItem data={i} /></SwiperSlide>)
+                )}
+            </Swiper>
         </section>
     );
 }
