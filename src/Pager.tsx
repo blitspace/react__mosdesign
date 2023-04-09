230f6219b4f9aa71e06da364e5bbe6530d5d08a1
ajcapacete <ajcapacete@gmail.com>
2023-04-09 16:13:17 +0800

    [master] Style Pager

--- src/Pager.tsx
+++ src/Pager.tsx
@@ -3,10 +3,13 @@ import { Link } from "react-router-dom";
 function Pager({ data, url }: { data: number[], url: string }) {
     return (
         <div>
-            <ul className="flex flex-row gap-2">
+            <ul className="flex flex-row justify-center gap-2">
                 {data.map(i => {
                     return <li key={i}>
-                        <Link to={`${url}/${i}`}>{i}</Link>
+                        <Link
+                            className="flex flex-row items-center justify-center w-8 h-8 text-white rounded-full bg-mos-footer hover:bg-mos-red hover:text-white"
+                            to={`${url}/${i}`}
+                        >{i}</Link>
                     </li>
                 })}
             </ul>
