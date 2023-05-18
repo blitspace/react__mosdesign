da9a1f07be089dbf562f44da67a2c5c3197f3488
ajcapacete <ajcapacete@gmail.com>
2023-05-18 18:51:01 +0800

    [master] Add scroll functionality to hero next section button

--- src/PageHero.tsx
+++ src/PageHero.tsx
@@ -9,12 +9,20 @@ function PageHero({ title, image, isLoading = false }: { title?: string, image?:
             backgroundImage: `url(\'${image}\')`,
         }}>
             <div></div>
+
             <div className="flex flex-row items-center justify-center text-5xl text-center text-white mt-mos-lg py-mos-md">
                 { isLoading && (<><Loader /><div className="mr-4" /></>) }
                 <span>{ title }</span>
             </div>
+
             <div className="text-center text-white py-mos-md">
-                <button className="text-white transition-all bg-white rounded-full bg-opacity-40 hover:bg-opacity-80 hover:text-mos-red">
+                <button
+                    className="text-white transition-all bg-white rounded-full bg-opacity-40 hover:bg-opacity-80 hover:text-mos-red"
+                    onClick={() => window.scrollTo({
+                        top: window.innerHeight,
+                        behavior: 'smooth'
+                    })}
+                >
                     <KeyboardArrowDownIcon fontSize="large" />
                 </button>
             </div>
